263213e9c75fc4b4a9f056d04ef37134e81079c7
Chu1503 <shivakash2003@gmail.com>
2024-08-09 01:24:45 +0530

    feat: new datetimepicker

--- app/main/add.jsx
+++ app/main/add.jsx
@@ -6,6 +6,8 @@ import {
   Text,
   TouchableOpacity,
   Platform,
+  Modal,
+  TouchableWithoutFeedback,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import Entypo from "@expo/vector-icons/Entypo";
@@ -14,21 +16,33 @@ import CustomField from "../../components/CustomField";
 import CustomModal from "../../components/CustomModal";
 import CustomPeriod from "../../components/CustomPeriod";
 import colors from "../../constants/colors";
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { useColorScheme } from 'react-native';
+import DatePicker from "react-native-modern-datepicker";
 
 const Add = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const [datePickerVisible, setDatePickerVisible] = useState(false);
-  const [selectedDate, setSelectedDate] = useState(new Date());
   const [price, setPrice] = useState("");
   const [selectedPeriod, setSelectedPeriod] = useState("");
   const [customPeriodVisible, setCustomPeriodVisible] = useState(false);
   const [customPeriodValue, setCustomPeriodValue] = useState("1");
   const [customPeriodUnit, setCustomPeriodUnit] = useState("Day");
   const [dropdownVisible, setDropdownVisible] = useState(false);
-  const colorScheme = useColorScheme();
-  const datePickerColor = colorScheme === 'dark' ? 'white' : 'black';
+
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDateChange = (date) => {
+    const [year, month, day] = date.split('/'); // Split the date string
+    const formattedDate = `${year}-${month}-${day}`; // Convert to YYYY-MM-DD format
+    const parsedDate = new Date(formattedDate);
+    if (!isNaN(parsedDate.getTime())) {
+      setSelectedDate(parsedDate);
+    } else {
+      console.warn("Invalid Date:", date);
+    }
+    setDatePickerVisible(false);
+  };
+  
+   
 
   const handleOpenModal = () => {
     console.log("Opening modal"); // Debug
@@ -44,12 +58,12 @@ const Add = () => {
     setDatePickerVisible(true);
   };
 
-  const handleDateChange = (event, date) => {
-    setDatePickerVisible(Platform.OS === 'ios' ? true : false);
-    if (date !== undefined) {
-      setSelectedDate(date);
-    }
-  };
+  // const handleDateChange = (event, date) => {
+  //   setDatePickerVisible(Platform.OS === "ios" ? true : false);
+  //   if (date !== undefined) {
+  //     setSelectedDate(date);
+  //   }
+  // };
 
   const handlePeriodSelect = (period) => {
     setSelectedPeriod(period);
@@ -98,7 +112,9 @@ const Add = () => {
             />
             <CustomField
               title="Billing Date"
-              placeholder={selectedDate ? selectedDate.toDateString() : "Select a payday"}
+              placeholder={
+                selectedDate ? selectedDate.toDateString() : "Select a payday"
+              }
               onPress={handleOpenDatePicker}
               otherStyles="my-2"
               innerText={
@@ -110,6 +126,7 @@ const Add = () => {
               }
               editable={false}
             />
+
             <CustomField
               title="Price"
               placeholder="0.00"
@@ -128,18 +145,20 @@ const Add = () => {
                 (period) => (
                   <TouchableOpacity
                     key={period}
-                    className={`flex-1 p-3 rounded-lg mx-1 mb-2 border-2 border-gray ${selectedPeriod === period
+                    className={`flex-1 p-3 rounded-lg mx-1 mb-2 border-2 border-gray ${
+                      selectedPeriod === period
                         ? "bg-secondary"
                         : "bg-black-100"
-                      }`}
+                    }`}
                     style={{ minWidth: 120 }}
                     onPress={() => handlePeriodSelect(period)}
                   >
                     <Text
-                      className={`text-center font-semibold ${selectedPeriod === period
+                      className={`text-center font-semibold ${
+                        selectedPeriod === period
                           ? "text-primary"
                           : "text-white"
-                        }`}
+                      }`}
                     >
                       {period}
                     </Text>
@@ -166,13 +185,55 @@ const Add = () => {
           )}
 
           {datePickerVisible && (
-            <DateTimePicker
-              value={selectedDate}
-              mode="date"
-              display="default"
-              onChange={handleDateChange}
-              textColor={datePickerColor}
-            />
+            <Modal
+              transparent
+              visible={datePickerVisible}
+              animationType="fade"
+              onRequestClose={() => setDatePickerVisible(false)}
+            >
+              <TouchableWithoutFeedback
+                onPress={() => setDatePickerVisible(false)}
+              >
+                <View className="flex-1 justify-center items-center bg-black/50">
+                  <View className="bg-[#090C08] rounded-xl p-4 w-4/5">
+                    {/* <DatePicker
+                      options={{
+                        backgroundColor: "#090C08",
+                        textHeaderColor: "#FFA25B",
+                        textDefaultColor: "#F6E7C1",
+                        selectedTextColor: "#fff",
+                        mainColor: "#F4722B",
+                        textSecondaryColor: "#D6C7A1",
+                        borderColor: "rgba(122, 146, 165, 0.1)",
+                      }}
+                      current={selectedDate.toISOString().split("T")[0]}
+                      selected={selectedDate.toISOString().split("T")[0]}
+                      mode="calendar"
+                      minuteInterval={30}
+                      style={{ borderRadius: 10 }}
+                      onDateChange={handleDateChange}
+                    /> */}
+                    <DatePicker
+                      options={{
+                        backgroundColor: "#090C08",
+                        textHeaderColor: "#FFA25B",
+                        textDefaultColor: "#F6E7C1",
+                        selectedTextColor: "#fff",
+                        mainColor: "#F4722B",
+                        textSecondaryColor: "#D6C7A1",
+                        borderColor: "rgba(122, 146, 165, 0.1)",
+                      }}
+                      current={selectedDate.toISOString().split("T")[0]}
+                      selected={selectedDate.toISOString().split("T")[0]}
+                      mode="calendar"
+                      minuteInterval={30}
+                      style={{ borderRadius: 10 }}
+                      onDateChange={handleDateChange}
+                    />
+                  </View>
+                </View>
+              </TouchableWithoutFeedback>
+            </Modal>
           )}
         </View>
       </ScrollView>
