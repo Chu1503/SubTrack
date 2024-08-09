d6c0b0b9e9c7c182f8c7bfd98a82b82803e2d509
Chu1503 <shivakash2003@gmail.com>
2024-08-09 17:20:29 +0530

    feat: logos, icons, splash screen

--- app/main/index.jsx
+++ app/main/index.jsx
@@ -1,35 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView, ScrollView, Image, View, Text, RefreshControl, TouchableOpacity } from 'react-native';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  Image,
+  View,
+  Text,
+  RefreshControl,
+  TouchableOpacity,
+} from "react-native";
+import axios from "axios";
 import { images } from "../../constants";
-import CustomCard from '../../components/CustomCard';
+import CustomCard from "../../components/CustomCard";
 import { useRouter } from "expo-router";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import SimpleLineIcons from "@expo/vector-icons/SimpleLineIcons";
 
 const calculateMonthlyPrice = (subscriptions) => {
   return subscriptions.reduce((total, subscription) => {
     let monthlyPrice = 0;
 
     switch (subscription.frequency) {
-      case 'monthly':
+      case "monthly":
         monthlyPrice = subscription.price;
         break;
-      case 'annually':
+      case "annually":
         monthlyPrice = subscription.price / 12;
         break;
       default:
-        if (subscription.frequency.startsWith('custom:')) {
-          const customFrequency = subscription.frequency.split(':')[1];
+        if (subscription.frequency.startsWith("custom:")) {
+          const customFrequency = subscription.frequency.split(":")[1];
           const unit = customFrequency.slice(-1);
           const amount = parseInt(customFrequency.slice(0, -1), 10);
 
-          if (unit === 'd' && amount) {
+          if (unit === "d" && amount) {
             monthlyPrice = (subscription.price / amount) * 30;
-          } else if (unit === 'm' && amount) {
+          } else if (unit === "m" && amount) {
             monthlyPrice = subscription.price / amount;
-          } else if (unit === 'y' && amount) {
+          } else if (unit === "y" && amount) {
             monthlyPrice = subscription.price / (amount * 12);
           }
         }
@@ -53,11 +61,11 @@ const Home = () => {
   }, []);
 
   const checkAuthStatus = async () => {
-    const user = await AsyncStorage.getItem('user');
+    const user = await AsyncStorage.getItem("user");
     if (!user) {
       setIsSignedIn(false);
-      router.replace('welcome');
-    } else{
+      router.replace("welcome");
+    } else {
       const userData = JSON.parse(user); // Parse the stored user data
       // Access the email property from userData (assuming it's stored there)
       const userName = userData.user.givenName;
@@ -68,7 +76,9 @@ const Home = () => {
 
   const fetchSubscriptions = async () => {
     try {
-      const response = await axios.get('http://68.233.114.188:5000/sub/991830116710023169');
+      const response = await axios.get(
+        "http://68.233.114.188:5000/sub/991830116710023169"
+      );
       setSubscriptions(response.data);
       setMonthlyPrice(calculateMonthlyPrice(response.data));
     } catch (error) {
@@ -93,10 +103,10 @@ const Home = () => {
     try {
       await GoogleSignin.revokeAccess();
       await GoogleSignin.signOut();
-      await AsyncStorage.removeItem('user');
+      await AsyncStorage.removeItem("user");
       setIsSignedIn(false);
     } catch (error) {
-      console.error('Error during logout:', error);
+      console.error("Error during logout:", error);
     }
   };
 
@@ -108,35 +118,49 @@ const Home = () => {
         }
       >
         <View className="w-full justify-center h-full px-4 my-6">
-          <View className="flex-row justify-between items-center mt-7">
-            
+          <View className="flex-1 flex-row justify-between items-center mt-7">
             <TouchableOpacity
               className="w-[40px] h-[40px] rounded-full justify-center items-center"
               onPress={handleLogout}
             >
               <Text className="text-primary text-3xl">
-              <SimpleLineIcons name="logout" size={24} color="red" />
+                <SimpleLineIcons name="logout" size={24} color="red" />
               </Text>
             </TouchableOpacity>
-            <Image source={images.logo} resizeMode='contain' className="w-[115px] h-[35px]" />
+            <View className="flex-1 justify-center items-center">
+              <Image
+                source={images.logo}
+                resizeMode="contain"
+                className="w-[150px] h-[35px]"
+              />
+            </View>
             <TouchableOpacity
               className="w-[60px] h-[40px] border-2 border-secondary rounded-full justify-center items-center"
-              onPress={() => router.push('/main/add')}
+              onPress={() => router.push("/main/add")}
             >
               <Text className="text-secondary text-3xl">+</Text>
             </TouchableOpacity>
           </View>
-          <Text className="text-xl text-white mt-7 font-pbold text-center">Hello <Text className="text-secondary font-pextrabold">{userName}</Text>!</Text>
-          <Text className="text-lg text-white text-regular mt-5 font-pmedium">Monthly Spending</Text>
-          <Text className="text-5xl text-secondary mt-1 mb-1 font-pextrabold pt-1">₹{monthlyPrice.toFixed(2)}</Text>
+
+          <Text className="text-xl text-white font-pbold text-center">
+            Hello{" "}
+            <Text className="text-secondary font-pextrabold">{userName}</Text>!
+          </Text>
+          <Text className="text-lg text-white text-regular mt-5 font-pmedium">
+            Monthly Spending
+          </Text>
+          <Text className="text-5xl text-secondary mt-1 mb-1 font-pextrabold pt-1">
+            ₹{monthlyPrice.toFixed(2)}
+          </Text>
           {/* <Text className="text-md text-white text-regular font-pmedium mb-2">Updated on XX/XX/XXXX</Text> */}
-          {subscriptions.map(subscription => (
+          {subscriptions.map((subscription) => (
             <CustomCard
               key={subscription.ID}
               platform={subscription.name}
               date={new Date(subscription.start_date).toLocaleDateString()}
               price={`₹${subscription.price}`}
-              onPress={() => router.push('/main/view')}            />
+              onPress={() => router.push("/main/view")}
+            />
           ))}
           <View className="mb-10"></View>
         </View>
--- constants/images.js
+++ constants/images.js
@@ -1,8 +1,9 @@
 import logo from "../assets/images/logo.png";
-import logoSmall from "../assets/images/logo-small.png";
+import logoIcon from "../assets/images/logo-icon.png"
+import logoText from "../assets/images/logo-text.png"
 import primevideo from "../assets/images/primevideo.png";
 import google from "../assets/images/google.png";
 import dash from "../assets/images/dash.png"
 import netflix from "../assets/images/netflix.png"
 
-export default { logo, logoSmall, primevideo, google, dash, netflix };
+export default { logo, logoIcon, logoText, primevideo, google, dash, netflix };
