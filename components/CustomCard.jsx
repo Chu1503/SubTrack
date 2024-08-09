6c9f2ee4a36fe205e0bc91fe656b750cf54b5589
ManavMuthanna <muthannamanav2003@gmail.com>
2024-08-09 23:34:47 +0530

    fix: icon based on platform for cards

--- components/CustomCard.jsx
+++ components/CustomCard.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, Image } from 'react-native';
-import { images } from "../constants"
+import { images } from "../constants";
 
 const CustomCard = ({ platform, date, price }) => {
-return (
+  // Helper function to determine image source based on platform
+  const getImageSource = (platform) => {
+    switch (platform.toLowerCase()) {
+      case 'primevideo':
+        return images.primevideo;
+      case 'netflix':
+        return images.netflix;
+      case 'disney+':
+        return images.disneyplus;
+      // Add more cases as needed
+      default:
+        return images.default_icon; // Fallback image
+    }
+  };
+
+  return (
     <TouchableOpacity className="min-h-[60px] bg-black-100 rounded-3xl shadow-md p-4 mt-3 border-2 border-gray">
       <View className="flex-row items-center">
-        <Image source={images.primevideo} className="w-[50px] h-[50px] mr-4" />
+        <Image source={getImageSource(platform)} className="w-[50px] h-[50px] mr-4" />
         
         <View className="flex-1">
           <View className="flex-row justify-between items-center">
@@ -20,7 +35,7 @@ return (
         </View>
       </View>
     </TouchableOpacity>
-    );
+  );
 };
 
-export default CustomCard
+export default CustomCard;
